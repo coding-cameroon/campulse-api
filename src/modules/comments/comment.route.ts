8cf792c5648b9f3c10b6e56df2e180eb0c8ccb85
coding-cameroon <codingcameroonweb@gmail.com>
2026-04-15 09:40:08 +0100

    comment route fixes

--- src/modules/comments/comment.route.ts
+++ src/modules/comments/comment.route.ts
@@ -5,8 +5,8 @@ import { commentController } from "./comment.controller.js";
 const router = Router();
 
 router.get("/:id", requireAuth, commentController.getComment);
-router.get("/:id/post", requireAuth, commentController.getComments);
 router.delete("/:id", requireAuth, commentController.deleteComment);
+router.get("/:postId/post", requireAuth, commentController.getComments);
 router.post("/:postId/create", requireAuth, commentController.createComment);
 
 export { router as commentRouter };
