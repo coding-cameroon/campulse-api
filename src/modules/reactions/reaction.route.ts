185a99823c8612825181e8abeb027d467b2fc241
coding-cameroon <codingcameroonweb@gmail.com>
2026-04-17 18:14:43 +0100

    reaction fix

--- src/modules/reactions/reaction.route.ts
+++ src/modules/reactions/reaction.route.ts
@@ -5,6 +5,6 @@ import { requireAuth } from "@/middlewares/auth.middleware.js";
 const router = Router();
 
 router.get("/:postId", requireAuth, reactionController.getReactions);
-router.post("/upsert", requireAuth, reactionController.toggleReaction);
+router.post("/upsert/:postId", requireAuth, reactionController.toggleReaction);
 
 export { router as reactionRouter };
